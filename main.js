dd3e4ed128030f1322e9c0a8e4301cac12a33df8
fer-tacosdecaca <130688038+fer-tacosdecaca@users.noreply.github.com>
2023-09-06 18:01:45 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,67 @@
 //https://teachablemachine.withgoogle.com/models/yNSbHuqVT/
 
 
+
 function startt(){
 navigator.mediaDevices.getUserMedia({audio: true})
-classy = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/yNSbHuqVT/model.json", modely)
+classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/yNSbHuqVT/model.json", modely)
 }
 
 function modely(){
 console.log("hfjckkhghhfiugbkgjhhkhfuhkjfhkfukghdkjjkhjhhgsugiug");
-classifier.classy(gotresults);
+classifier.classify(gotresults);
 }
 
 function gotresults(error, results){
+if (error) {
+    console.error(error);
+} else 
+    
 console.log(results);
-}
+    document.getElementById("result_label").innerHTML = 'Escucho -' + [0].label;
+document.getElementById("result_confidence").innerHTML = 'precicion -' +(results[0].confidence*100).toFixed(2)+"%";
+ 
+    img =document.getElementById("alien1")
+    img1 =document.getElementById("alien2")
+    img2 =document.getElementById("alien4")
+    img3 =document.getElementById("alien4")
+    
+    if(results[0].label == "pou"){  
+    img2.src = "aliens-03.gif"
+    img1.src = "aliens-02.png"
+    img.src = "aliens-01.png"
+    img3.src = "aliens-04.png"
+
+   
+   
+    }
+    else if(results[0].label == "A"){  
+    img.src = "aliens-01.gif"
+    img1.src = "aliens-02.png"
+    img2.src = "aliens-03.png"
+    img3.src = "aliens-04.png"
+
+
+    }
+    
+    else if(results[0].label == "I"){  
+    img2.src = "aliens-02.gif"
+    img.src = "aliens-01.png"
+    img2.src = "aliens-03.png"
+    img3.src = "aliens-04.png"
+
+
+    }
+
+
+
+
+
+
+
+
+
+    
+} 
+
+
